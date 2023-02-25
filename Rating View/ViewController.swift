68fe520c3ca844e4a1265a5cd6b4d3d9bc2a8ce6
Krati Mittal <krati@nickelfox.co>
2023-02-25 22:56:24 +0530

    Initial Commit

---
+++ Rating View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rating View
+//
+//  Created by Krati Mittal on 25/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
